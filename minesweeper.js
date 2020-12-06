267835ebcedd142dfbc636287626e92690155a7a
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2020-12-07 11:13:32 +1300

    Add Counting the bomb function

--- minesweeper.js
+++ minesweeper.js
@@ -2,19 +2,76 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {cells: [
-    {row: 0, col: 0, isMine: true, hidden: true},
-    {row: 0, col: 1, isMine: false, hidden: true},
-    {row: 0, col: 2, isMine: false, hidden: true},
-    {row: 1, col: 0, isMine: false, hidden: true},
-    {row: 1, col: 1, isMine: true, hidden: true},
-    {row: 1, col: 2, isMine: false, hidden: true},
-    {row: 2, col: 0, isMine: false, hidden: true},
-    {row: 2, col: 1, isMine: false, hidden: true},
-    {row: 2, col: 2, isMine: true, hidden: true}
+    {
+      row: 0,
+      col: 0,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 0,
+      col: 1,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 0,
+      col: 2,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 0,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 1,
+      isMine: true,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 2,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 0,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 1,
+      isMine: false,
+      hidden: true,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 2,
+      isMine: true,
+      hidden: true,
+      surroundingMines: 0
+    }
   ]
 };
 
 function startGame () {
+  for (let i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -39,5 +96,13 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  let count = 0;
+  let surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
+  for (let i = 0; i < surroundingCells.length; i++) {
+    if (surroundingCells[i].isMine === true) {
+      count += 1;
+    }
+  }
+  return cell.surroundingMines = count;
 }
 
